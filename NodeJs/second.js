fa5062c443090fdfaa8f6bf479829942657f7fe9
GautamR24 <rawatgautam24@protonmail.com>
2023-03-30 14:19:25 +0530

    learnt about nodeJs wrapper funtion

--- NodeJs/second.js
+++ NodeJs/second.js
@@ -2,5 +2,11 @@ test = {
     name : "testingName",
     color : "testingColor"
 }
-
+console.log(exports,require,module,__filename,__dirname);
 module.exports = test
+
+/**
+ * this is module wrapper function, node js wraps every module export inside this type
+ * of functions
+ */
+//function(exports,require,module,__filename,__dirname)
