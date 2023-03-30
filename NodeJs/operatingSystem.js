41222c507e15cb9d862f8e24d27f86d156b9dce1
GautamR24 <rawatgautam24@protonmail.com>
2023-03-30 14:30:34 +0530

    learnt some functions of OS module

---
+++ NodeJs/operatingSystem.js
@@ -0,0 +1,11 @@
+//For learning about different functions related to OS in NodeJs
+
+const os = require('os');
+
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.uptime());
+console.log(os.networkInterfaces());
